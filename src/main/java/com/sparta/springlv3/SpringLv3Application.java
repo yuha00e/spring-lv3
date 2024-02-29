1e01aae5fafafca151755672ee69ff744dfd5bb0
yuha <yhj230508@gmail.com>
2024-02-29 15:32:01 +0900

    first commit

---
+++ src/main/java/com/sparta/springlv3/SpringLv3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springlv3/SpringLv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
